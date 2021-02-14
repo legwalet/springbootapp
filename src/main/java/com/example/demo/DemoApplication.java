b49260e25108d7dd74d53c3797b9a31086a736f8
legwalet <71738517+legwalet@users.noreply.github.com>
2021-02-14 19:15:03 +0200

    Update DemoApplication.java

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,13 +1,14 @@
 package com.example.demo;
 
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-@SpringBootApplication
-public class DemoApplication {
+@RestController
+public class HelloController {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+	@RequestMapping("/")
+	public String index() {
+		return "Greetings from Spring Boot!";
 	}
 
 }
