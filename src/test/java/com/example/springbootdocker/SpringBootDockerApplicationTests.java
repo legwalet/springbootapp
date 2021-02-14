4fbdb506d916ecd1e92f6857dec6eb91a9ad8179
legwalet <71738517+legwalet@users.noreply.github.com>
2021-02-14 19:31:45 +0200

    alter application folders

--- src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
+++ src/test/java/com/example/springbootdocker/SpringBootDockerApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.demo;
+package com.example.springbootdocker;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class SpringBootDockerApplicationTests {
 
 	@Test
 	void contextLoads() {
